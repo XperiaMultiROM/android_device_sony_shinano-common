27ef919518062fb69bac34d0538a6c4ee3df8bbd
Christian Oder <myself5@carbonrom.org>
2016-03-13 22:12:01 +0100

    shinano: update framebuffer with Nexus 6p framebuffer
    This framebuffer works with 3.4 and 3.10 Kernel

--- PlatformConfigOmni.mk
+++ PlatformConfigOmni.mk
@@ -23,9 +23,9 @@ TARGET_KERNEL_SOURCE := kernel/sony/msm
 
 #MultiROM config. MultiROM also uses parts of TWRP config
 MR_INPUT_TYPE := type_b
-MR_INIT_DEVICES := device/sony/shinano-common/multirom/init_devices.c
+MR_INIT_DEVICES := device/sony/shinano-common/multirom/mr_init_devices.c
 MR_USE_QCOM_OVERLAY := true
-MR_QCOM_OVERLAY_HEADER := device/sony/shinano-common/multirom/framebuffer_qcom_overlay.h
+MR_QCOM_OVERLAY_HEADER := device/sony/shinano-common/multirom/mr_qcom_overlay.h
 MR_KEXEC_DTB := true
 MR_FSTAB := device/sony/shinano-common/rootdir/twrp.fstab
 MR_USE_MROM_FSTAB := true
