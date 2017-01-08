0d410a2151e79df60493eb7d890d8cdbb397e854
Alexander Diewald <Diewi@diewald-net.com>
2017-01-08 11:51:32 +0100

    multirom: Enhance stock compatibility (prevent mounting apps_log)
    In N roms that use the Sony AOSP init implementation, the apps_log
    partition is used to store some temporal data for the AOSP
    recovery. This partition ist not used in AOSP roms, but by stock
    roms, and modifications to it prevent booting stock roms if the
    apps_log partition is not wiped before.
    
    With this commit the fstab is modified when multirom is started
    such that any line containing the apps_log string is removed prior
    to booting the rom. Hence, modifications to the apps_log partition
    are prevented for all roms (primary & secondary), which allows to
    conveniently use stock and newer custom roms in parallel.

--- multirom/mr_hooks.c
+++ multirom/mr_hooks.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mount.h>
 
 #include <cutils/klog.h>
@@ -31,6 +32,36 @@ void mrom_hook_before_fb_close(void) {}
 #endif
 
 #if MR_DEVICE_HOOKS >= 3
+static int remove_line_file(const char* filename, const char* search_str) {
+    char* outFileName = "tmpfile";
+    FILE* inFile = fopen(filename, "r");
+    FILE* outFile = fopen(outFileName, "w+");
+    char line [1024];
+
+    if( inFile == NULL ) {
+        return -1;
+    }
+
+    while( fgets(line, sizeof(line), inFile) != NULL ) {
+        if( strstr(line, search_str) == NULL ) {
+            fprintf(outFile, "%s", line);
+        }
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+
+    // possible you have to remove old file here before
+    if( remove(filename) ) {
+        return -1;
+    }
+    if( rename(outFileName, filename) ) {
+        return -1;
+    }
+
+    return 0;
+}
+
 void tramp_hook_before_device_init(void)
 {
 	// Mount the securityfs for disabling sony_ric.
@@ -59,6 +90,10 @@ void tramp_hook_before_device_init(void)
 			fclose(fd);
     	}
     }
+
+    // Prevent mounting the apps_log partition whose use would break stock roms.
+    //system("sed -i -e 's/\\/dev\\/block\\/bootdevice\\/by-name\\/apps_log.*\\/misc/#\\/dev\\/block\\/bootdevice\\/by-name\\/apps_log\\1\\/misc/g' /fstab.shinano");
+    remove_line_file("/fstab.shinano", "apps_log");
 }
 #endif
 
